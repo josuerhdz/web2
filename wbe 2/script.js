d7bd6e4dedfea0c18bb6545c998e230d48b14a2c
josuerhdz <131608940+josuerhdz@users.noreply.github.com>
2023-09-04 21:16:09 -0600

    Add files via upload

---
+++ wbe 2/script.js
@@ -0,0 +1,7 @@
+function showInfo(sneaker) {
+    sneaker.querySelector('.sneaker-info').style.display = 'block';
+}
+
+function hideInfo(sneaker) {
+    sneaker.querySelector('.sneaker-info').style.display = 'none';
+}
